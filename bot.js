90c4be3473ebebda3aaf195725210b2c63a18559
iandtrapp <idt37414@uga.edu>
2015-07-22 10:37:24 -0400

    Debug log

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ function respond() {
       botRegex = /^\/Ian$|^\/Katie$|^\/Swindon$|^\/Claire$|^\/Aaron$|^\/Daniel$|^\/Nick$|^\/Lauren$|^\/Sara$|^\/Kara$/;
 
   if(request.text && botRegex.test(request.text)) {
+    console.log(request.text);
     this.res.writeHead(200);
     postMessage();
     this.res.end();
