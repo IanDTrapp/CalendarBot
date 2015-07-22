8baf5d2c1acb9f1695fb4618d2fac25ed0de0334
iandtrapp <idt37414@uga.edu>
2015-07-22 19:35:49 -0400

    regex changes

--- bot.js
+++ bot.js
@@ -5,8 +5,9 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/Ian|^\/Katie|^\/Kara|^\/Swindon|^\/Claire$|^\/Aaron$|^\/Daniel$|^\/Nick$|^\/Lauren$|^\/Sara$|^\/All$|^\/Help$/;
+  var reg = new RegExp(/^(Ian|Katie|Kara|Swindon|Claire|Aaron|Daniel|Nick|Lauren|Sara|Help$|All$)/, 'i');
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && reg.test(request.text)) {
     this.res.writeHead(200);
     var date = new Date(request.created_at * 1000);
     console.log(date);
