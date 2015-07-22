1a844ae2f9f0894a3cc80236d386e420a9105fb2
iandtrapp <idt37414@uga.edu>
2015-07-22 17:31:51 -0400

    Regex

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = new RegExp(/^\/Ian|^\/Katie|^\/Kara|^\/Swindon|^\/Claire$|^\/Aaron$|^\/Daniel$|^\/Nick$|^\/Lauren$|^\/Sara$|^\/All$|^\/Help$/, 'i');
+      botRegex = (/^\/Ian|^\/Katie|^\/Kara|^\/Swindon|^\/Claire$|^\/Aaron$|^\/Daniel$|^\/Nick$|^\/Lauren$|^\/Sara$|^\/All$|^\/Help$/, 'i');
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
