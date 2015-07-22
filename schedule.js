f0be0d92d199b4e2248fee99a07459974fcf99c4
iandtrapp <idt37414@uga.edu>
2015-07-22 14:09:25 -0400

    Place functionality

--- schedule.js
+++ schedule.js
@@ -4,27 +4,27 @@ module.exports = {
             class1: {
                 name: 'CSCI 3030',
                 time: '8:00AM - 8:35AM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'CSCI 3030 Breakout',
                 time: '8:50AM - 9:55AM',
-                location: ''
+                place: ''
             },
             class3: {
                 name: 'ENGL 3590W',
                 time: '11:15AM - 12:05PM',
-                location: ''
+                place: ''
             },
             class4: {
                 name: 'GEOG 1111',
                 time: '1:25PM - 2:15PM',
-                location: ''
+                place: ''
             },
             class5: {
                 name: 'CSCI 2720',
                 time: '3:35PM - 4:25PM',
-                location: ''
+                place: ''
             }
 
         },
@@ -32,48 +32,48 @@ module.exports = {
             class1: {
                 name: 'GEOG 1111 Lab',
                 time: '11:00AM - 1:45PM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'CSCI 2720',
                 time: '3:30PM - 4:45PM',
-                location: ''
+                place: ''
             }
         },
         3: {
             class1: {
                 name: 'CSCI 3030',
                 time: '8:00AM - 8:35AM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'ENGL 3590W',
                 time: '11:15AM - 12:05PM',
-                location: ''
+                place: ''
             },
             class3: {
                 name: 'GEOG 1111',
                 time: '1:25PM - 2:15PM',
-                location: ''
+                place: ''
             }
         },
         4: {
             class1: {
                 name: 'CSCI 2720',
                 time: '3:30PM - 4:45PM',
-                location: ''
+                place: ''
             }
         },
         5: {
             class1: {
                 name: 'ENGL 3590W',
                 time: '11:15AM - 12:05PM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'GEOG 1111',
                 time: '1:25PM - 2:15PM',
-                location: ''
+                place: ''
             }
         }
     },
@@ -83,70 +83,70 @@ module.exports = {
             class1: {
                 name: 'CHEM 1211',
                 time: '12:20PM - 1:10PM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'BIOL 1108',
                 time: '2:30PM - 3:20PM',
-                location: ''
+                place: ''
             },
             class3: {
                 name: 'PHYS 1111',
                 time: '4:40PM - 5:30PM',
-                location: ''
+                place: ''
             }
         },
         2: {
             class1: {
                 name: 'BIOL 1108 Lab',
                 time: '12:30PM - 4:25PM',
-                location: ''
+                place: ''
             }
         },
         3: {
             class1: {
                 name: 'CHEM 1211',
                 time: '12:20PM - 1:10PM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'BIOL 1108',
                 time: '2:30PM - 3:20PM',
-                location: ''
+                place: ''
             },
             class3: {
                 name: 'PHYS 1111',
                 time: '4:40PM - 5:30PM',
-                location: ''
+                place: ''
             }
         },
         4: {
             class1: {
                 name: 'CHEM 1211 Lab',
                 time: '12:30PM - 3:15PM',
-                location: ''
+                place: ''
             }
         },
         5: {
             class1: {
                 name: 'PHYS 1111 Lab',
                 time: '10:10AM - 12:05PM',
-                location: ''
+                place: ''
             },
             class2: {
                 name: 'CHEM 1211',
                 time: '12:20PM - 1:10PM',
-                location: ''
+                place: ''
             },
             class3: {
                 name: 'BIOL 1108',
                 time: '2:30PM - 3:20PM',
-                location: ''
+                place: ''
             },
             class4: {
                 name: 'PHYS 1111',
                 time: '4:40PM - 5:30PM',
-                location: ''
+                place: ''
             }
         }
     },
@@ -204,90 +204,90 @@ module.exports = {
             class1: {
                 name: 'MATH 4000',
                 time: '10:10AM - 11:00AM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class2: {
                 name: 'MATH 2700',
                 time: '12:20PM - 1:10PM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class3: {
                 name: 'EMAT 3700',
                 time: '1:25PM - 3:15PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             },
             class4: {
                 name: 'EMAT 4800 Lab',
                 time: '3:20PM - 4:25PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             }
         },
         2: {
             class1: {
                 name: 'MATH 4600',
                 time: '11:00AM - 12:15PM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class2: {
                 name: 'EMAT 4800',
                 time: '2:00PM - 4:45PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             }
         },
         3: {
             class1: {
                 name: 'MATH 4000',
                 time: '10:10AM - 11:00AM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class2: {
                 name: 'MATH 2700',
                 time: '12:20PM - 1:10PM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class3: {
                 name: 'EMAT 3700',
                 time: '1:25PM - 3:15PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             },
             class4: {
                 name: 'EMAT 4800 Lab',
                 time: '3:20PM - 4:25PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             }
         },
         4: {
             class1: {
                 name: 'MATH 4600',
                 time: '11:00AM - 12:15PM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class2: {
                 name: 'EMAT 4800',
                 time: '2:00PM - 4:45PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             }
         },
         5: {
             class1: {
                 name: 'MATH 4000',
                 time: '10:10AM - 11:00AM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class2: {
                 name: 'MATH 2700',
                 time: '12:20PM - 1:10PM',
-                location: 'Boyd'
+                place: 'Boyd'
             },
             class3: {
                 name: 'EMAT 3700',
                 time: '1:25PM - 3:15PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             },
             class4: {
                 name: 'EMAT 4800 Lab',
                 time: '3:20PM - 4:25PM',
-                location: 'Aderhold'
+                place: 'Aderhold'
             }
         }
     },
