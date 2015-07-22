e2807b9fcf0558046762dcb99f9916178eb40fe8
iandtrapp <idt37414@uga.edu>
2015-07-22 10:25:20 -0400

    Adding name recognition

--- bot.js
+++ bot.js
@@ -5,7 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/Ian$/ || /^\/Katie$/
+          || /^\/Swindon$/ || /^\/Claire$/
+          || /^\/Aaron$/ || /^\/Daniel$/
+          || /^\/Nick$/ || /^\/Lauren$/
+          || /^\/Sara$/ || /^\/Kara$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
